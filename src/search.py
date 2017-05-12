a0481f76fcde918b8e7468b5ba51d616f4014e88
rss316 <Ry8893gmail.com>
2017-05-12 05:58:35 -0400

    Added capability to run other information, other than name. Statement if found nothing

--- src/search.py
+++ src/search.py
@@ -9,10 +9,17 @@ s = Search(using=es)
 def search(term):
     res = es.search(index="_all", body={"query": {"query_string": {"query": term}}})
     source = []
+    url = []
     for doc in res['hits']['hits']:
         if doc['_source']['filename'] not in source:
             source.append(doc['_source']['filename'])
+            for dataURL in doc['_source']['datasetDistribution']:
+                url.append(dataURL)
 
-    print("%d documents found" % len(source))
-    print(json.dumps(source))
-
+    if(len(source)>0):
+        print("%d documents found" % len(source))
+        for index, item in enumerate(source):
+            #print("Document Found: %s \t URL Associated: %s" % (item , url[index]))
+            print("Document Found: %s" % item )
+    else:
+        print("No documents containin the query: %s were found" % term)
