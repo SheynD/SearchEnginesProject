3fa07db9bee6dfaada620a67add3c3c07d175339
rss316 <Ry8893gmail.com>
2017-05-13 05:14:33 -0400

    Added aggregate function to pull an aggregation of keywords

--- src/search.py
+++ src/search.py
@@ -23,3 +23,25 @@ def search(term):
             print("Document Found: %s" % item )
     else:
         print("No documents containin the query: %s were found" % term)
+
+def searchUI():
+    #Performs an aggregate of all Keywords
+    my_body = {
+   "size": 0, 
+   "aggregations": {
+      "Keywords": {
+         "terms": {
+            "field": "Keywords"
+            , "size": 10
+             }
+          }
+       }
+    }
+    aggr = []
+    res = es.search(index="_all", body=my_body)
+    for doc in res['aggregations']['Keywords']['buckets']:
+        aggr.append(doc)
+    for word in aggr:
+        print("Keyword: %-15s Frequency: %s" % (word['key'],word['doc_count']))
+
+
